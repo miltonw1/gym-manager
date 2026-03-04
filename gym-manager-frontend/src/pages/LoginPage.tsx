1044722111f99f81799119812b582e0f65208305
Milton Wery <milton_st@hotmail.com>
2026-03-04 20:19:47 -0300

    refactor(ui): Move components to pages and implement MainLayout

---
+++ gym-manager-frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/auth/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted/50 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
